205296e865d41a3ed41f5dc095c7e37eefbf73d5
Rodrigo Giraldi <rgiraldisilva@gmail.com>
2019-02-03 12:04:12 -0500

    Initial commit

---
+++ src/main/java/com/rodrigo/TaskManager.java
@@ -0,0 +1,14 @@
+package com.rodrigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManager {
+    public static void main(String[] args) {
+
+        SpringApplication.run(TaskManager.class, args);
+
+        System.out.println("Running...");
+    }
+}
